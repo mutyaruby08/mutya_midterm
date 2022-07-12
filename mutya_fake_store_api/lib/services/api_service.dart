b4740e6e3222e85a23d5e5d67f6f768418a2111f
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-07-12 15:58:31 +0800

    Created api service getAllCategories() and getAllProducts()

--- mutya_fake_store_api/lib/services/api_service.dart
+++ mutya_fake_store_api/lib/services/api_service.dart
@@ -1,3 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<List<String>> getAllCategories() {
+    return http.get(Uri.parse('$baseUrl/products/categories')).then((data) {
+      final categories = <String>[];
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+
+        for (var item in jsonData) {
+          categories.add(item);
+        }
+      }
+      return categories;
+    }).catchError((err) => print(err));
+  }
 }
