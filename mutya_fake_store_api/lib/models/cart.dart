a4465ecd8b481ef89c4c8745912bb5efa5d5ff11
Ruby Mae Mutya <ruby.mutya@usjr.edu.ph>
2022-07-12 14:10:19 +0800

    Added models

---
+++ mutya_fake_store_api/lib/models/cart.dart
@@ -0,0 +1,15 @@
+import 'package:mutya_fake_store_api/models/products.dart';
+
+class Cart {
+  int id;
+  int userId;
+  DateTime date;
+  List<Products> products;
+
+  Cart({
+    required this.id,
+    required this.userId,
+    required this.date,
+    required this.products,
+  });
+}
